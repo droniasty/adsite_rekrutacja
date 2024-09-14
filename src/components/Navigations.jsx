da0eefc4a39042183b7eb0d5597efa75d7fd3b20
Maciej <maciejdronka235@wp.pl>
2024-09-14 11:18:57 +0200

    car spot image

--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import CarSpot from './assets/images/my-image.png';
 
 const Navigations = () => {
   return (
     <div className="w-[1440px] h-[79px] gap-0 flex justify-between items-center justify-center">
       <div className="w-[1200px] h-[47px] flex justify-between items-center gap-0 mx-auto">
       <div className="w-[300px] h-[29.58px]">
-        <img src="/CarsSpot.png" alt="logo"  className="w-full h-full object-contain"/>
+        <img src={CarSpot} alt="logo"  className="w-full h-full object-contain"/>
       </div>
       <div className="w-[144px] h-[24px] flex gap-[24px]">
         <p className="w-[91px] h-[24px] font-robotoFlex text-[15px] font-normal leading-[24px] text-center">
